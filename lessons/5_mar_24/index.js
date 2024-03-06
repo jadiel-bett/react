4eda1917015966438642f75e48279393efc6ddbf
jadiel_bett <jedielbett@gmail.com>
2024-03-06 22:49:59 +0300

    Lesson 5th March 2024

---
+++ lessons/5_mar_24/index.js
@@ -0,0 +1,11 @@
+// 5th March 2024
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Gallery from './Profile';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Gallery />
+  </React.StrictMode>
+);
