1c62376f24b11d0c81747cde5e47a67764e6c7fa
jadiel_bett <jedielbett@gmail.com>
2024-03-06 23:00:33 +0300

    Challenge 3: Passing JSX in a 'children' prop

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import { getImageUrl } from './utils.js';
-
-function Avatar({ person, size }) {
-    var letter = size < 90 ? 's': 'b';
-  return (
-    <img
-      className="avatar"
-      src={getImageUrl(person, letter)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  );
-}
-
 export default function Profile() {
-  return (
-    <Avatar
-      size={60}
-      person={{ 
-        name: 'Gregorio Y. Zara', 
-        imageId: '7vQD0fP'
-      }}
-    />
-  );
-}
+    return (
+      <div>
+        <div className="card">
+          <div className="card-content">
+            <h1>Photo</h1>
+            <img
+              className="avatar"
+              src="https://i.imgur.com/OKS67lhm.jpg"
+              alt="Aklilu Lemma"
+              width={70}
+              height={70}
+            />
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-content">
+            <h1>About</h1>
+            <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
