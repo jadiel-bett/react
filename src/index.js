d87f5645a0bbcc83a861a32e10abd33a60435c7d
jadiel_bett <jedielbett@gmail.com>
2024-03-08 13:26:04 +0300

    Conditional rendering

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-// 6th March 2024
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Profile from './App';
+import DrinkList from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Profile />
+    <DrinkList />
   </React.StrictMode>
 );
